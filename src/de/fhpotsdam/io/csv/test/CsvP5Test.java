6a2f05c80a88007f129ab6914c01ffa131c9afcb
Tim Pulver <tim_pulver@gmx.de>
2012-11-15 23:40:12 +0100

    Added test (needs to be commented for lib to compile)

--- src/de/fhpotsdam/io/csv/test/CsvP5Test.java
+++ src/de/fhpotsdam/io/csv/test/CsvP5Test.java
@@ -2,9 +2,14 @@
 //
 //import static org.junit.Assert.*;
 //
+//import java.util.ArrayList;
+//import java.util.concurrent.ArrayBlockingQueue;
+//
 //import org.junit.Test;
 //
 //import de.fhpotsdam.io.csv.CsvP5;
+//import de.fhpotsdam.io.csv.analyzer.DataType;
+//import de.fhpotsdam.io.csv.analyzer.StructureAnalyzer;
 //
 //import processing.core.PApplet;
 //
@@ -16,4 +21,14 @@
 //		csv.load();
 //	}
 //
+//	@Test
+//	public void testGetPossibleDataTypes(){
+//		String s1 = "False";
+//		StructureAnalyzer a = new StructureAnalyzer();
+//		ArrayList<DataType> al = a.getPossibleDataTypes(s1);
+//		for(DataType dt: al){
+//			println(dt.toString());
+//		}
+//		
+//	}
 //}
